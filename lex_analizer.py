7b58d5aefe072b32d4bdd75d82f11281bafaa5ca
Bruno Venâncio de Souza e Silva <brunosilva.5934@aluno.saojudas.br>
2023-11-16 23:19:50 -0300

    Adicionado algumas palavras reservadas e operadores

--- lex_analizer.py
+++ lex_analizer.py
@@ -1,28 +1,51 @@
 import ply.lex as lex
 
+# lista de palavras reservadas
+reserved = {
+    'se': 'P_RESERVADA_SE',
+    'então': 'P_RESERVADA_ENTAO',
+    'senão': 'P_RESERVADA_SENAO',
+    'retorne': 'P_RESERVADA_RETORNE',
+    'verdadeiro': 'P_RESERVADA_VERDADEIRO',
+    'falso': 'P_RESERVADA_FALSO',
+    'imprima': 'P_RESERVADA_IMPRIMA',
+    'int': 'P_RESERVADA_TIPO_INTEIRO'
+}
+
 # lista de tokens
-tokens = (
-    'INTEIRO',
-    'ADICAO',
-    'SUBTRACAO',
-    'MULTIPLICACAO',
-    'DIVISAO',
+tokens = [
+    'NUM_INTEIRO',
+    'SIMBOL_ADICAO',
+    'SIMBOL_SUBTRACAO',
+    'SIMBOL_MULTIPLICACAO',
+    'SIMBOL_DIVISAO',
+    'MAIORQUE',
+    'MAIOROUIGUAL',
+    'MENORQUE',
+    'MENOROUIGUAL',
     'ABRE_PAREN',
     'FECHA_PAREN',
-    'COMENTARIO'    
-)
+    'SIMBOL_COMENTARIO',
+    'ID',
+    'SIMBOL_ATRIBUICAO'
+ ] + list(reserved.values())
 
 # expressões regulares para os tokens mais simples
-t_ADICAO = r'\+'
-t_SUBTRACAO = r'-'
-t_MULTIPLICACAO = r'\*'
-t_DIVISAO = r'/'
+t_SIMBOL_ADICAO = r'\+'
+t_SIMBOL_SUBTRACAO = r'-'
+t_SIMBOL_MULTIPLICACAO = r'\*'
+t_SIMBOL_DIVISAO = r'/'
 t_ABRE_PAREN = r'\('
 t_FECHA_PAREN = r'\)'
 t_ignore = ' \t'
+t_SIMBOL_ATRIBUICAO = r'\='
+t_MAIORQUE = r'\>'
+t_MENORQUE = r'\<'
+t_MAIOROUIGUAL = r'\>\='
+t_MENOROUIGUAL = r'\<\='
 
 # expressões regulares que exigem ações adicionais são definidas usando uma função
-def t_INTEIRO(t):
+def t_NUM_INTEIRO(t):
     r'\d+'
     t.value = int(t.value)
     return t
@@ -33,9 +56,15 @@ def t_novalinha(t):
     t.lexer.lineno += len(t.value)
 
 # ignora as linha de comentários
-def t_COMENTARIO(t):
+def t_SIMBOL_COMENTARIO(t):
     r'\#.*'
     pass
+
+# função para identificar variáveis
+def t_ID(t):
+    r'[a-zA-Z][a-z-A-Z0-9çÇãÃ]*'
+    t.type = reserved.get(t.value, 'ID') # Verifica se é uma palavra reservada
+    return t
 
 # função para lidar com erros, por hora apenas ignora
 def t_error(t):
